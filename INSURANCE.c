124d5f08c352d4ec2ba6d145d3abe661a68109c7
Ritu Raj Singh <rs2969@srmist.edu.in>
2022-07-22 22:37:34 +0530

    q1 solved

---
+++ INSURANCE.c
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin >> n;
+	while(n--)
+	{
+	    cin >> a >> b;
+	    cout << min(a,b) << endl;;
+	}
+	return 0;
+}
