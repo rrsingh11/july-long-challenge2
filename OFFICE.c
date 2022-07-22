1bc6a3f43dfe0785a01705423c6c4f0ff42896c4
Ritu Raj Singh <rs2969@srmist.edu.in>
2022-07-22 22:38:16 +0530

    q2 solved

---
+++ OFFICE.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b;
+	cin >> t;
+	while(t--)
+	{
+	    cin >> a >> b;
+	    cout << (a*4)+b << endl; 
+	}
+	return 0;
+}
